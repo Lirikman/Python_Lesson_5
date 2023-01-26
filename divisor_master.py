b52f2caa13561c9ff597c6a17eb3c237c8f7edb1
Дмитрий <999gothicman999@mail.ru>
2023-01-26 21:05:03 +0700

    Task 1, 2, 3 completed

--- divisor_master.py
+++ divisor_master.py
@@ -3,16 +3,20 @@
 
 def prime_numbers(x):
     if x>0 and x<1000:
+        if x == 1:
+            return 'Число ' + str(x) + ' не является простым'
         for i in range(2, (x//2)+1):
             if x % i == 0:
-                return 'Число не является простым'
+                return 'Число ' + str(x) + ' не является простым'
         else:
-            return 'Число является простым'
+            return 'Число ' + str(x) + ' является простым'
     else:
         return 'Ошибка. Пожалуйста введите число от 1 до 1000'
 
 if __name__ == '__main__':
-    print(prime_numbers(12))
+    print(prime_numbers(5))
+    print(prime_numbers(40))
+    print(prime_numbers(34))
 
 #2. выводит список всех делителей числа
 def number_divisor(x):
@@ -28,18 +32,29 @@ def number_divisor(x):
         return 'Ошибка. Пожалуйста введите число от 1 до 1000'
 
 if __name__ == '__main__':
-    print(number_divisor(35))
+    print(number_divisor(5))
+    print(number_divisor(40))
+    print(number_divisor(34))
 
 #3. выводит самый большой простой делитель числа
 def max_prime_divisor(x):
-    prime_divisors = []
-    number_divisor(x)
-    for i in divisor:
-        for l in range(2, ((i//2)+1)):
-            if l % i == 0:
+    if x > 0 and x < 1000:
+        prime_divisors = []
+        number_divisor(x)
+        for i in divisor:
+            if i == 1:
                 continue
+            for l in range(2, (i//2)+1):
+                if i % l == 0:
+                    break
             else:
                 prime_divisors.append(i)
-    return prime_divisors
+        result = 'Самый большой простой делитель числа ' + str(x) + ' : ' + str(prime_divisors[-1])
+        return result
+    else:
+        return 'Ошибка. Пожалуйста введите число от 1 до 1000'
 
-print(max_prime_divisor(35))
+if __name__ == '__main__':
+    print(max_prime_divisor(5))
+    print(max_prime_divisor(34))
+    print(max_prime_divisor(40))
